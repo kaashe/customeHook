43828c96a54ea2ebb6475e689ec88efeca8fff46
Kashif Ullah <113616575+Kashif-Ullah1@users.noreply.github.com>
2024-01-03 14:41:05 +0500

    grab the code

---
+++ README.md
@@ -0,0 +1,7 @@
+this react js app is aimed to create custom hooks for redundant logic like fetching data from db.
+#clone the repository
+#navigate to the project folder
+#open it in a code editor or simply go to cmd or terminal(ubuntu)
+#npm i or yarn
+#npm start or yarn start
+#happy coding ....
---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import useHttp from './hooks/use-http';
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const transformTask = (task) => {
+    const loadedTasks = [];
+
+    for (const taskKey in task) {
+      loadedTasks.push({ id: taskKey, text: task[taskKey].text });
+    }
+
+    setTasks(loadedTasks);
+  };
+ const {isLoading,error,sendRequest:fetchTasks } = useHttp({url:"https://custome-hook-50c0f-default-rtdb.firebaseio.com/tasks.json"},transformTask)
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
